70c8a5918375388f32f710aa64d27077397e6c56
gordonwu66 <gordonwu66@gmail.com>
2019-01-16 07:33:17 -0500

    count and setTime

--- timer.py
+++ timer.py
@@ -1,15 +1,34 @@
 import time
 
-print('Enter anything to start timer: ')
-input()
-startTime = time.time()
-print('Enter anything to stop timer: ')
-input()
-endTime = time.time()
+def count():
+	print('Enter anything to start timer: ')
+	input()
+	startTime = time.time()
+	print('Enter anything to stop timer: ')
+	input()
+	endTime = time.time()
 
-timeElapsed = endTime - startTime
-minutes = int(timeElapsed / 60)
-seconds = int(timeElapsed % 60)
+	timeElapsed = endTime - startTime
+	minutes = int(timeElapsed / 60)
+	seconds = int(timeElapsed % 60)
+	print(str(minutes) +' minute(s) and ' +str(seconds) +' second(s) have passed.')
 
-print(str(minutes) +' minute(s) and ' +str(seconds) +' second(s) have passed.')
+def setTime():
+	print('Enter number of seconds to count down from: ')
+	timeStr = input()
+	timeInt = int(timeStr)
+	while timeInt > 0:
+		print(timeInt)
+		time.sleep(1)
+		timeInt -= 1
+	print('Done!')
 
+def main():
+	print('Start counting time (1) or set a time to count (2)?: ')
+	answer = input()
+	if(answer == '1'):
+		count()
+	elif(answer == '2'):
+		setTime()
+
+main()
